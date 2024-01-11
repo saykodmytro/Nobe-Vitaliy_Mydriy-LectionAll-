78cd7a8e139dd513a5bf20ef42571d5fec463086
Dmytro Sayko <saykodmytro@icloud.com>
2024-01-11 22:02:43 +0200

    Update book.js

--- Modul_3-Lesson_06/controllers/book.js
+++ Modul_3-Lesson_06/controllers/book.js
@@ -1,23 +1,32 @@
 const Book = require("../models/book");
 
 const getAllBooks = async (req, res, next) => {
-  const books = await Book.find({ genre: "Horor", favorite: true });
-  res.send(books);
+  try {
+    const books = await Book.find();
+    //{ genre: "Action", favorite: true }
+    res.send(books);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const getBook = async (req, res, next) => {
   const { id } = req.params;
 
-  const book = await Book.findById(id);
+  try {
+    const book = await Book.findById(id);
 
-  if (book === null) {
-    return res.status(404).send("Book not found");
+    if (book === null) {
+      return res.status(404).send("Book not found");
+    }
+
+    res.send(book);
+  } catch (error) {
+    next(error);
   }
-
-  res.send(book);
 };
 
-const createBook = (req, res, next) => {
+const createBook = async (req, res, next) => {
   const book = {
     title: req.body.title,
     author: req.body.author,
@@ -25,24 +34,74 @@ const createBook = (req, res, next) => {
     year: req.body.year,
   };
 
-  const result = await Book.create(book);
+  try {
+    const result = await Book.create(book);
 
-  res.send("Create Book");
+    res.status(201).send(result);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const updateBook = (req, res, next) => {
+const updateBook = async (req, res, next) => {
   const { id } = req.params;
-  res.send(` Update Book ${id}`);
+
+  const book = {
+    title: req.body.title,
+    author: req.body.author,
+    genre: req.body.genre,
+    year: req.body.year,
+  };
+
+  try {
+    const result = await Book.findByIdAndUpdate(id, book, { new: true });
+
+    if (result === null) {
+      return res.status(404).send("Book not found");
+    }
+
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const deleteBook = (req, res, next) => {
+const deleteBook = async (req, res, next) => {
   const { id } = req.params;
-  res.send(` Delete Book ${id}`);
+
+  try {
+    const result = await Book.findByIdAndDelete(id);
+
+    if (result === null) {
+      return res.status(404).send("Book not found");
+    }
+
+    res.send({ id });
+  } catch (error) {
+    next(error);
+  }
 };
 
-const changeBookFavorite = (req, res, next) => {
+const changeBookFavorite = async (req, res, next) => {
   const { id } = req.params;
-  res.send(` Change Book ${id} Favorire`);
+
+  try {
+    const result = await Book.findByIdAndUpdate(
+      id,
+      {
+        favorite: req.body.favorite,
+      },
+      { new: true }
+    );
+
+    if (result === null) {
+      return res.status(404).send("Book not found");
+    }
+
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
